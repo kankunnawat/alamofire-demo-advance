065e9edd4f6402944a2b13b51d8a488e59a3936e
kan.k <kan.k@siriustech.io>
2023-06-20 21:58:18 +0700

    .gitignore is now working

--- starter/GitOnFire/Networking/Oauth/GitHubConstants.swift
+++
@@ -1,7 +0,0 @@
-enum GitHubConstants {
-  static let clientID = "b26132e4d578fd16db4e"
-  static let clientSecret = "8c0e2a2915f6cdda541bfdc31ddac26cb4fe1520"
-  static let redirectURI = "www.google.com"
-  static let scope = "kankunnawat"
-  static let authorizeURL = "https://github.com/login/oauth/authorize"
-}
